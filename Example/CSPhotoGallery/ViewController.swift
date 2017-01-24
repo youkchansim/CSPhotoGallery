d84c00be4242a552465f4ef53d3cfb43b5247533
Chansim Youk <dbrckstla@naver.com>
2017-01-25 00:24:47 +0900

    Fix Example

--- Example/CSPhotoGallery/ViewController.swift
+++ Example/CSPhotoGallery/ViewController.swift
@@ -11,15 +11,11 @@ import CSPhotoGallery
 import Photos
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func btnAction(_ sender: Any) {
         let designManager = CSPhotoDesignManager.instance
         
         //  Main
         designManager.photoGalleryOKButtonTitle = "OK"
-//        designManager.isOKButtonHidden = true
         
         let vc = CSPhotoGalleryViewController.instance
         vc.delegate = self
@@ -28,20 +24,23 @@ class ViewController: UIViewController {
 //        present(vc, animated: true)
         navigationController?.pushViewController(vc, animated: true)
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 }
 
 extension ViewController: CSPhotoGalleryDelegate {
     func getAssets(assets: [PHAsset]) {
-        print(assets.count)
-    }
-    
-    func dismiss() {
-        dismiss(animated: true, completion: nil)
+        assets.forEach {
+            let size = CGSize(width: $0.pixelWidth, height: $0.pixelHeight)
+            PhotoManager.sharedInstance.assetToImage(asset: $0, imageSize: size, completionHandler: nil)
+        }
     }
 }
